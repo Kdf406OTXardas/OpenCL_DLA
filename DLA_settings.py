42091fca005fa3adf1266a485f7e4657dd7259ea
Kdf406OTXardas <n.a.tkachov@yandex.ru>
2023-06-14 03:18:56 +0300

    with_wait_sinhronization

--- DLA_settings.py
+++ DLA_settings.py
@@ -12,26 +12,25 @@ queue = cl.CommandQueue(context)
 
 
 TARGET_POROSITY = 10
-FIELD_SIZE = 2
+FIELD_SIZE = 50
 TARGET_PORE_CELLS = TARGET_POROSITY / 100 * FIELD_SIZE * FIELD_SIZE
 field_test = np.zeros((FIELD_SIZE + 2,
                        FIELD_SIZE + 2),
-                      dtype=np.uint16)
+                      dtype=np.uint8)
 field_test_2 = np.full_like(field_test,
                             fill_value=0,
-                            dtype=np.uint16)
+                            dtype=np.uint8)
 print(field_test_2)
 
 
 mf = cl.mem_flags 
 field_test_cl = cl.Buffer(context,
                              mf.READ_WRITE | mf.COPY_HOST_PTR,
-                             hostbuf = field_test)
+                             hostbuf = 640*480*field_test)
 field_test_cl_2 = cl.Buffer(context,
-                             mf.READ_WRITE | mf.COPY_HOST_PTR,
-                             hostbuf = field_test_2)
+                             mf.WRITE_ONLY | mf.USE_HOST_PTR,
+                             hostbuf = 640*480*field_test_2)
 
-#   res_g[lid_x][lid_y] = a_g[gid_x][gid_y] + 1; 
 
 program = cl.Program(context, """ 
 __kernel void testCl(__global const int **a_g,
@@ -41,18 +40,21 @@ __kernel void testCl(__global const int **a_g,
   
   int i, j;
   
-  for (i = 0; i < 2; i++){
-      for (j = 0; j < 2; j++){
+  for (i = 0; i < 50; i++){
+      for (j = 0; j < 50; j++){
           res_g[i][j] = a_g[i][j] + 1;
       }
   }
-    
 } 
 """).build()
 
 field_test_out = program.testCl(queue, field_test.shape, None, field_test_cl, field_test_cl_2)
-# a_1 = 1
-# a_2 = 1
+field_test_out.wait()
+a_1 = 1
+a_2 = 1
 # print(f'field_test[{a_1}][{a_2}] = {field_test_out[a_1][a_2]}')
-a = field_test_out
+a = np.empty_like(field_test)
+# cl._enqueue_read_buffer(queue, field_test_cl_2, a).wait()
+# events = [field_test_out]
+cl.enqueue_copy(queue, a, field_test_cl_2)
 print(a)
